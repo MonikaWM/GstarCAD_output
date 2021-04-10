01f81498f9b18c8eeb0af676551a8c6a4d0a0564
Monika <monikawilczynskamitura@gmail.com>
2021-04-10 23:06:14 +0200

    Initial commit

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,18 @@
+define(function() {var keywords=[{w:"GstarCAD",p:["p0"]},{w:"instrukcja",p:["p0"]},{w:"Dostosowanie",p:["p1"]},{w:"programu",p:["p1"]},{w:"Edycja",p:["p2"]},{w:"rysunku",p:["p2","p7","p8","p9","p10"]},{w:"Interfejs",p:["p3"]},{w:"u\u017Cytkownika",p:["p3"]},{w:"Pierwsze",p:["p4"]},{w:"kroki",p:["p4"]},{w:"Wprowadzenie",p:["p5"]},{w:"Instalacja",p:["p6"]},{w:"Otwieranie",p:["p7"]},{w:"Przywracanie",p:["p8"]},{w:"Tworzenie",p:["p9"]},{w:"Zapis",p:["p10"]}];
+var ph={};
+ph["p0"]=[0, 1];
+ph["p1"]=[2, 3];
+ph["p2"]=[4, 5];
+ph["p3"]=[6, 7];
+ph["p4"]=[8, 9];
+ph["p5"]=[10];
+ph["p6"]=[11];
+ph["p7"]=[12, 5];
+ph["p8"]=[13, 5];
+ph["p9"]=[14, 5];
+ph["p10"]=[15, 5];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {6:-1,1:4,9:2,10:2,8:2,4:-1,5:-1,7:2,3:4,2:-1};
+});
